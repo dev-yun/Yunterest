b3a455dee2b1d71c8be57009aae2a71fa5877fbf
dev-yun <yunpro2766@gmail.com>
2021-08-03 00:02:14 +0900

    Django course 21

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
+# path를 지정할때 FBV인 hello_world는 그냥 hello_world로 받아오지만
+# CBV인 create는 AccountCreateView.as_view() 라는 as_view()를 명시해줘야한다.
