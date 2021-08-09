4637220a7eaf85569ecfe7756f1ede7d26158c82
dev-yun <yunpro2766@gmail.com>
2021-08-09 13:41:47 +0900

    Profile create

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    # forms.py의 form을 받아와 사용
+    # forms.py에서 user.id를 넣으면 보안상 문제가 발생하기때문에 views에서 처리
+    def form_valid(self, form):
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
