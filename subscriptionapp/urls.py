e9e5d243e521e3db61d24441dcac65d673c64140
dev-yun <yunpro2766@gmail.com>
2021-08-18 17:46:06 +0900

    Create SubscriptionApp

---
+++ subscriptionapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from subscriptionapp.views import SubscriptionView
+
+app_name = 'subscriptionapp'
+
+urlpatterns=[
+    path('subscription/', SubscriptionView.as_view(), name='subscription')
+]
