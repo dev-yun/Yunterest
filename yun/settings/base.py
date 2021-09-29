76edd063a39cbd083d5f68d7c7892ac97c919725
dev-yun <yunpro2766@gmail.com>
2021-09-30 00:38:42 +0900

    Add messages framework

--- yun/settings/base.py
+++ yun/settings/base.py
@@ -14,6 +14,7 @@ import environ
 import os
 from pathlib import Path
 
+from django.contrib.messages import constants as messages
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +49,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
 
 ROOT_URLCONF = 'yun.urls'
 
